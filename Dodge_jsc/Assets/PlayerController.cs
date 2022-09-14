ec06319ab1b705d57872d873cb9681418d73b15c
DARKBLACK0709 <jsc3217@naver.com>
2022-09-14 17:37:50 +0900

    20220914

--- Dodge_jsc/Assets/PlayerController.cs
+++ Dodge_jsc/Assets/PlayerController.cs
@@ -10,12 +10,27 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        { playerRigidbody.AddForce(0f, 0f, speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            playerRigidbody.AddForce(0f, 0f, -speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        { playerRigidbody.AddForce(speed, 0f, 0f); }
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            playerRigidbody.AddForce(-speed, 0f, 0f);
+        }
     }
+
+    
 }
