922ca866334dae08925407dac78a8c2533e7714e
DARKBLACK0709 <jsc3217@naver.com>
2022-09-20 13:13:37 +0900

    20200920_01

--- Dodge_jsc/Assets/Bullet.cs
+++ Dodge_jsc/Assets/Bullet.cs
@@ -7,8 +7,40 @@ public class Bullet : MonoBehaviour
     public float speed = 8f;
     private Rigidbody bulletRigidbody;
     // Update is called once per frame
-    void Update()
+
+    void Start()
+    { //게임 오브젝트에서 Rigidbody 컴포넌트를 찾아
+        // bulletRigidbody에 할당
+        bulletRigidbody = GetComponent<Rigidbody>();
+        // 리지드 바디 속도 = 앞쪽 방향 * 이동 속력 
+        bulletRigidbody.velocity = transform.forward * speed;
+        //new Vector3()
+
+        // 3초 뒤에 자신의 게임 오브젝트 파괴
+        // 메소드
+        Destroy(gameObject, 3f);
+    }
+
+
+
+   // void Update()
+    //{
+  
+    //}
+    // 트리거 충돌 시 자동으로 실행되는 메서드
+
+    private void OnTriggerEnter(Collider other)
+    //충돌한 상대방 게임 오브젝트가 Player 태그를 가졌다면,
     {
-        
+        if (other.tag == "player")
+        {
+            PlayerController playerController =
+                 other.GetComponent<PlayerController>();
+
+            if (playerController != null)
+            {
+                playerController.Die();
+            }
+        }
     }
 }
--- Dodge_jsc/Assets/PlayerController.cs
+++ Dodge_jsc/Assets/PlayerController.cs
@@ -7,31 +7,16 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRigidbody;
     public float speed = 8f;
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        playerRigidbody = GetComponent < Rigidbody > ();
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
-    //{
-        //if (Input.GetKey(KeyCode.UpArrow) == true)
-        //{
-        //    playerRigidbody.AddForce(0f, 0f, speed);
-        //}
-        //if (Input.GetKey(KeyCode.DownArrow) == true)
-        //{
-        //    playerRigidbody.AddForce(0f, 0f, -speed);
-        //}
-        //if (Input.GetKey(KeyCode.RightArrow) == true)
-        //{ playerRigidbody.AddForce(speed, 0f, 0f); }
-
-        //if (Input.GetKey(KeyCode.LeftArrow) == true)
-        //{
-        //    playerRigidbody.AddForce(-speed, 0f, 0f);
-        //}
+    void Update() 
         {
+        
             float xInput = Input.GetAxis("Horizontal");
             float zInput = Input.GetAxis("Vertical");
 
@@ -41,13 +26,18 @@ public class PlayerController : MonoBehaviour
             Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
 
             playerRigidbody.velocity = newVelocity;
+        
+
+        /*
+         */
+      
+         //   playerRigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, 0f, Input.GetAxis("Vertical") * speed);
         }
-   /// }
 
 
-    public void Die()
-    {
-        gameObject.SetActive(false);
-    }
-    
-}
+        public void Die()
+        {
+            gameObject.SetActive(false);
+        }
+
+    } 
